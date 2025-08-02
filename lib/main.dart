6223d0aef63d3fc9189710d6f2e08bef10f8ed5a
Arjun <engarjunbarman@gmail.com>
2025-08-02 23:50:20 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -95,6 +95,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter ',
+      debugShowCheckedModeBanner: false,
       home: Home(),
     );
   }
