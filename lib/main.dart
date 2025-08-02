f78bde950ddd4a0b2756465efffca2065c0272fb
Arjun <engarjunbarman@gmail.com>
2025-08-02 23:37:29 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,88 @@
+// This is the first exam
+
+/*
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter ',
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Exam'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Hello, World!',
+              style: TextStyle(
+                color: Colors.red,
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Welcome to Flutter!',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 20),
+            Image.network('https://upload.wikimedia.org/wikipedia/commons/1/17/Google-flutter-logo.png'),
+            SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green, // button background color
+              ),
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Button Pressed!')),
+                );
+              },
+              child: Text('Press Me'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+// This is the second  exam
+
+
+
+
+
 import 'package:flutter/material.dart';
 
 void main() {
